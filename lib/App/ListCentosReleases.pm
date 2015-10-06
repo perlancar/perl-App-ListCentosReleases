76fff5b1db5b2c7ef3e99f9c726b80eb839425ef
perlancar (@pc-office) <perlancar@gmail.com>
2015-10-06 19:58:11 +0700

    Initial commit

---
+++ lib/App/ListCentosReleases.pm
@@ -0,0 +1,13 @@
+package App::ListCentosReleases;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: List CentOS releases (CLI)
+
+=head1 DESCRIPTION
+
+See the included script L<list-centos-releases>.
+
+=cut
